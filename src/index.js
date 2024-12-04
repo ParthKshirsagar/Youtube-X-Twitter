738a0385d38a41958ff8827d661e8a36976b7d14
Parth Kshirsagar <parthkshirsagar96@gmail.com>
2024-12-05 00:39:05 +0530

    Added some utilities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/connection.js";
 
+import app from './app.js';
+
 dotenv.config({
     path: "../env"
 })
 
 connectDB()
+.then(() => {
+    const alternatePort = 8000
+    app.listen(process.env.PORT || alternatePort, () => {
+        console.log(`App listening on port: ${process.env.PORT || alternatePort}`)
+    });
+    app.on('error', (error) => {
+        console.error("Error: ", error);
+        throw error
+    })
+})
+.catch((e) => {
+    console.log("Database connection failed!! ", e)
+})
